483ed831120e42d114cfb4351e674112d4a77bf1
ryanbrembo <ryanalamsyah39@gmail.com>
2026-02-03 18:16:39 +0700

    animate card

--- src/app/fonts/page/Home/HomePage.tsx
+++ src/app/fonts/page/Home/HomePage.tsx
@@ -1,13 +1,33 @@
 'use client'
 
 import { ArrowRight } from "lucide-react";
+import { useEffect } from "react";
 import CustomImage from "@/app/ui/CustomImage";
 import ShinyText from "@/components/ShinyText";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const HeroSection = () => {
+  const controls = useAnimation();
+    const { ref, inView } = useInView({ triggerOnce: true });
+    const {refLeft, leftInView} = useInView({triggerOnce: true});
+  
+    useEffect((): void => {
+      if (inView) controls.start({ opacity: 1, x: 0});
+    }, [inView]);
+     useEffect((): void => {
+      if (leftInView) controls.start({ opacity: 1, x: 0});
+    }, [leftInView]);
   return (
     <>
       <div className="min-h-screen  md:grid grid-cols-2  duration-300 ease-in-out pt-20 md:pt-8">
+         <motion.div
+        ref={ref}
+        initial={{opacity: 0, x: -600,}}
+        animate={controls}
+        transition={{duration: 0.6}}
+        className="flex justify-center"
+        >
         <div className="flex flex-col gap-4 md:px-12 px-4 justify-center">
           <h1 className="text-slate-600 font-bold text-4xl pb-2 md:pb-8">
            <span>Senyum Sehat dan </span>
@@ -57,6 +77,14 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
+        </motion.div>
+ <motion.div
+        ref={refLeft}
+        initial={{opacity: 0, x: 600,}}
+        animate={controls}
+        transition={{duration: 0.6}}
+        className="flex justify-center"
+        >
         <div className="flex  justify-center items-center mt-8 md:mt-0">
           <CustomImage
             src="/Hero.jpg"
@@ -65,6 +93,7 @@ const HeroSection = () => {
             height={370}
           />
         </div>
+        </motion.div>
       </div>
     </>
   );
